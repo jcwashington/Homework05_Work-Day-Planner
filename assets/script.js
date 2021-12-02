857c72756a603719c67d25b6c1512c73c010d767
JCWashington <jasmine.washington412@gmail.com>
2021-12-02 12:39:52 -0500

    updated js to change color of row depending on current time

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,24 @@
 $( document ).ready(function() {
-    // Handler for .ready() called.
-    let today = moment().format("MMM Do YY");
+    // Handler for .ready() called. https://api.jquery.com/ready/#ready-handler
+    let today = moment().format('ll');;
     let displayToday = document.getElementById("currentDay");
     displayToday.innerHTML = today;
     // need to know what hour we are in immediately
-    currentHr = moment().format("HH");
+    var currentHr = moment().format("HH");
+
+    //grabs hour from each time slot and compares it to actual time
+    $(".time-block").each(function () {
+        var sectionHr = parseInt($(this).attr("id").split("hour")[1]);
+        console.log(sectionHr);
+
+        if (currentHr == sectionHr) {
+        $(this).addClass("present");
+        $(this).children(".description").addClass("white-text");
+        } else if (currentHr < sectionHr) {
+        $(this).removeClass("present");
+        $(this).addClass("future");
+        } else if (currentHr > sectionHr) {
+        $(this).removeClass("future");
+        $(this).addClass("past")};
   });
+});
