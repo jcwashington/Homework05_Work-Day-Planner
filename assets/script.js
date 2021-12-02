f0ea0ff70f14b573e6c2a91a4f70104635a6d8d1
JCWashington <jasmine.washington412@gmail.com>
2021-12-02 13:43:32 -0500

    store text to localStorage and retrieve it on refresh

--- assets/script.js
+++ assets/script.js
@@ -6,7 +6,20 @@ $( document ).ready(function() {
     // need to know what hour we are in immediately
     var currentHr = moment().format("HH");
 
-    //grabs hour from each time slot and compares it to actual time
+    // retreive tasks from localStorage and put them in the correct places
+    $("#hour08 .description").val(localStorage.getItem("08"));
+    $("#hour09 .description").val(localStorage.getItem("09"));
+    $("#hour10 .description").val(localStorage.getItem("10"));
+    $("#hour11 .description").val(localStorage.getItem("11"));
+    $("#hour12 .description").val(localStorage.getItem("12"));
+    $("#hour13 .description").val(localStorage.getItem("13"));
+    $("#hour14 .description").val(localStorage.getItem("14"));
+    $("#hour15 .description").val(localStorage.getItem("15"));
+    $("#hour16 .description").val(localStorage.getItem("16"));
+    $("#hour17 .description").val(localStorage.getItem("17"));
+    $("#hour18 .description").val(localStorage.getItem("18"));
+
+    //compares hour from each time slot and compares it currentHr to determine block color
     $(".time-block").each(function () {
         var sectionHr = parseInt($(this).attr("id").split("hour")[1]);
         console.log(sectionHr);
@@ -21,4 +34,20 @@ $( document ).ready(function() {
         $(this).removeClass("future");
         $(this).addClass("past")};
   });
+
+    //save text input to localStorage https://api.jquery.com/click/
+    //$( "#target" ).click(function() {
+    //  alert( "Handler for .click() called." );
+    //});
+    
+    $( ".saveBtn").click( function(event) {
+        event.preventDefault(); // prevent the default refresh
+        var taskText = $(this).siblings(".description").val();
+        var taskTime = $(this).parent().attr("id").split("hour")[1];
+
+        console.log(taskText, taskTime); 
+        localStorage.setItem(taskTime, taskText);
+    })
+
+
 });
